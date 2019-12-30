6ea7e520c6acd4f909024d2f9b3d3f76939c25dc
wangxiaofengx <605280610@qq.com>
2019-12-30 23:27:22 +0800

    ..

--- boot/src/main/java/com/mrwang/controller/UserController.java
+++ boot/src/main/java/com/mrwang/controller/UserController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("user")
+@RestController()
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
