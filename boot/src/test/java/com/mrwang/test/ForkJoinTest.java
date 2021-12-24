76ef7dca66498e5c0ab1d48e2b371aa3b9e404d9
wangxiaofeng <605280610@qq.com>
2021-12-24 23:52:56 +0800

    测试

--- boot/src/test/java/com/mrwang/test/ForkJoinTest.java
+++ boot/src/test/java/com/mrwang/test/ForkJoinTest.java
@@ -5,10 +5,8 @@ import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.Future;
-import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ForkJoinTest extends RecursiveTask {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -17,6 +15,9 @@ public class ForkJoinTest extends RecursiveTask {
     private int end;
     private List<Integer> datas;
     private int threshold = 10;
+
+    private static volatile int count = 0;
+    private static AtomicInteger atomicInteger = new AtomicInteger();
 
     public ForkJoinTest(List<Integer> datas, int start, int end) {
         this.datas = datas;
@@ -32,20 +33,22 @@ public class ForkJoinTest extends RecursiveTask {
         ForkJoinPool forkJoinPool = new ForkJoinPool();
 
         List<Integer> list = new ArrayList<>();
-        int length = 30;
+        int length = 100;
         for (int i = 0; i < length; i++) {
             list.add((int) (Math.random() * length));
         }
-
-        ForkJoinTest forkJoinTest = new ForkJoinTest(list, 0, length-1);
+        System.out.println(Thread.currentThread().getId() + ":" + Thread.currentThread().getName());
+        ForkJoinTest forkJoinTest = new ForkJoinTest(list, 0, length - 1);
         Future f = forkJoinPool.submit(forkJoinTest);
-        System.out.println(f.get());
+        System.out.println("max:" + f.get() + " active:" + Thread.activeCount());
+        System.out.println(count + "\t" + atomicInteger.get() + "\t" + Thread.currentThread().getId() + ":" + Thread.currentThread().getName());
     }
 
 
     @Override
     protected Object compute() {
-
+        count++;
+        System.out.println(count + "\t" + atomicInteger.getAndIncrement() + "\t" + Thread.currentThread().getId() + ":" + Thread.currentThread().getName());
         if (this.end - this.start < threshold) {
             int max = 0;
             for (int i = start; i <= end; i++) {
@@ -58,13 +61,18 @@ public class ForkJoinTest extends RecursiveTask {
         ForkJoinTest right = new ForkJoinTest(this.datas, middle + 1, this.end);
         left.fork();
         right.fork();
-        invokeAll(left,right);
+        invokeAll(left, right);
         return Math.max((int) left.join(), (int) right.join());
     }
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ForkJoinTest forkJoinTest = new ForkJoinTest();
-//        System.out.println((int) (Math.random() * 1000000));
+        System.out.println((int) (Math.random() * 1000000));
         forkJoinTest.test();
+        System.out.println(atomicInteger.get());
+        System.out.println(atomicInteger.incrementAndGet());
+        System.out.println(atomicInteger.incrementAndGet());
+        System.out.println(atomicInteger.get());
+
     }
 }
